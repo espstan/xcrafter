d1f05e425fd4c7efc985c323d61fc4274ae1b29b
Dmitriy <golub.d.a@yandex.ru>
2019-04-18 13:27:37 +0300

    Загрузка каталога товара с данными из БД

--- ru.xcrafter/app/routes.py
+++ ru.xcrafter/app/routes.py
@@ -2,12 +2,15 @@ from flask import render_template, request, redirect, url_for, send_from_directo
 from werkzeug.security import generate_password_hash
 from app import app, db
 from app.models import Users, Products
+from db.get_all_products import get_all_products
 
 
 @app.route('/')
 @app.route('/index')
 def index() -> 'html':
-    return render_template('Index.html')
+    products = get_all_products()
+    return render_template('Index.html',
+                           products=products)
 
 
 @app.route('/api/1/user', methods=['POST'])
