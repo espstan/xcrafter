4fe2634be563995a0f7a668b5212573cbe088fbb
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-04-26 11:20:29 +0300

    добавлена конфигурация для почты

--- ru.xcrafter/config.py
+++ ru.xcrafter/config.py
@@ -5,9 +5,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Super Secret Key'
-    #
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #                           'sqlite:///' + os.path.join(basedir, 'app.db')
 
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:example@localhost:5432'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
