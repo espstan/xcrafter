a3a2000001e4258517d83a661542dc90385d2455
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-05-14 17:56:18 +0300

    ISSUE-158 Добавление установки Docker и Docker-compose в скрипт
    Добавлена установка docker и docker-compose в
    скрипт /bin/prod_server_config.sh

--- ru.xcrafter/bin/prod_server_config.sh
+++ ru.xcrafter/bin/prod_server_config.sh
@@ -3,6 +3,8 @@
 ssh xcrafter.ru << EOF
 apt-get update
 apt install -y nginx python3-pip virtualenv letsencrypt
+wget -O - https://get.docker.com | bash -
+pip3 install docker-compose
 
 echo "Port 1990" >> /etc/ssh/sshd_config
 systemctl restart ssh
