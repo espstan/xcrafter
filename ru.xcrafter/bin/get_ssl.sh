bddd5e37bd5b6325ee62dc90557449d0a9fd83ef
Dmitriy <golub.d.a@yandex.ru>
2019-05-14 16:43:45 +0300

    ISSUE-129 Изменил скрипт для получения SSL

--- ru.xcrafter/bin/get_ssl.sh
+++ ru.xcrafter/bin/get_ssl.sh
@@ -1,9 +1,5 @@
-apt-get update
-apt-get install -y nginx
-apt-get install software-properties-common
-add-apt-repository universe
-add-apt-repository ppa:certbot/certbot
-apt-get update
-apt-get install -y certbot python-certbot-nginx
+#!/bin/bash
 
-sudo certbot --nginx -d www.xcrafter.ru -d static.xcrafter.ru -d api.xcrafter.ru -d m.xcrafter.ru
+service nginx stop
+certbot certonly --standalone -d xcrafter.ru
+service nginx start
