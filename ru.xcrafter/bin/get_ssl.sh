070c8e0cd154544441b29ee805dd65e5d8d3e867
Dmitriy <golub.d.a@yandex.ru>
2019-05-14 16:52:28 +0300

    ISSUE-129 Добавил адрес, изменил форматирование адресов

--- ru.xcrafter/bin/get_ssl.sh
+++ ru.xcrafter/bin/get_ssl.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 service nginx stop
-certbot certonly --standalone -d www.xcrafter.ru -d static.xcrafter.ru -d api.xcrafter.ru -d m.xcrafter.ru
+letsencrypt certonly --standalone \
+-d www.xcrafter.ru \
+-d static.xcrafter.ru \
+-d api.xcrafter.ru \
+-d m.xcrafter.ru \
+-d xcrafter.ru
 service nginx start
 
