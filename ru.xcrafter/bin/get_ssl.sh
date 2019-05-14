9376f1e0c0e3b3d66853509578cb7a630f6f366d
Dmitriy <golub.d.a@yandex.ru>
2019-05-14 16:55:57 +0300

    ISSUE-129 Изменил порядок адресов

--- ru.xcrafter/bin/get_ssl.sh
+++ ru.xcrafter/bin/get_ssl.sh
@@ -2,10 +2,10 @@
 
 service nginx stop
 letsencrypt certonly --standalone \
+-d xcrafter.ru \
 -d www.xcrafter.ru \
 -d static.xcrafter.ru \
 -d api.xcrafter.ru \
--d m.xcrafter.ru \
--d xcrafter.ru
+-d m.xcrafter.ru
 service nginx start
 
