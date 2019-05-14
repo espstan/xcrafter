fca2b13e14f9e3681479bd02ae2e2090fca2d0c4
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-05-14 16:18:57 +0300

    ISSUE-128 Перенесение всех установок в один "apt install"

--- ru.xcrafter/bin/prod_server_config.sh
+++ ru.xcrafter/bin/prod_server_config.sh
@@ -2,10 +2,7 @@
 
 ssh xcrafter.ru << EOF
 apt-get update
-apt install -y nginx
-apt install -y python3-pip
-apt install -y virtualenv
-apt install -y letsencrypt
+apt install -y nginx python3-pip virtualenv letsencrypt
 
 echo "Port 1990" >> /etc/ssh/sshd_config
 systemctl restart ssh
