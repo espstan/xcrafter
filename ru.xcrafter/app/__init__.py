f799175ef63f85ab09b264b34bc103bb09db025f
espstan <espstan@gmail.com>
2019-05-15 17:57:20 +0300

    ISSUE-176 Добавлен Bundle для css

--- ru.xcrafter/app/__init__.py
+++ ru.xcrafter/app/__init__.py
@@ -23,6 +23,7 @@ api = Api(app)
 login = LoginManager(app)
 login.login_view = 'index'
 mail = Mail(app)
+
 assets = Environment(app)
 js = Bundle('script/toggle-sign-mode.js',
             'script/check-form.js',
@@ -30,11 +31,22 @@ js = Bundle('script/toggle-sign-mode.js',
             'script/main-page.js',
             filters='jsmin', output='bundle.min.js')
 assets.register('js_all', js)
+
 assets_vendors = Environment(app)
 js_vendors = Bundle('static/lib/bootstrap/4.3.1/js/bootstrap.min.js',
                     '/static/lib/jquery/3.4.0/jquery-3.4.0.min.js',
                     filters='jsmin', output='vendors.min.js')
 assets_vendors.register('js_vendors', js_vendors)
+
+assets_css = Environment(app)
+css = Bundle('static/css/main-page.css',
+             'static/css/main-page.css',
+             '/static/css/cart.css',
+             '/static/css/popup-window.css',
+             '/static/css/profile.css',
+             filters='cssmin', output='bundle.min.css')
+assets_css.register('css_all', css)
+
 
 from app import routes, models, server_api
 
