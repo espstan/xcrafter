1878f9e1cc2f909eb02c354028a487af8f5c17e4
espstan <espstan@gmail.com>
2019-05-15 18:44:07 +0300

    ISSUE-176 Оставлена одна сущность assets (экземпляр Environment)

--- ru.xcrafter/app/__init__.py
+++ ru.xcrafter/app/__init__.py
@@ -25,6 +25,7 @@ login.login_view = 'index'
 mail = Mail(app)
 
 assets = Environment(app)
+
 js = Bundle('script/toggle-sign-mode.js',
             'script/check-form.js',
             'script/modal-windows.js',
@@ -32,20 +33,18 @@ js = Bundle('script/toggle-sign-mode.js',
             filters='jsmin', output='bundle.min.js')
 assets.register('js_all', js)
 
-assets_vendors = Environment(app)
 js_vendors = Bundle('lib/bootstrap/4.3.1/js/bootstrap.min.js',
                     'lib/jquery/3.4.0/jquery-3.4.0.min.js',
                     filters='jsmin', output='vendors.min.js')
-assets_vendors.register('js_vendors', js_vendors)
+assets.register('js_vendors', js_vendors)
 
-assets_css = Environment(app)
 css = Bundle('css/main-page.css',
              'css/signup-form.css',
              'css/cart.css',
              'css/popup-window.css',
              'css/profile.css',
              filters='cssmin', output='bundle.min.css')
-assets_css.register('css_all', css)
+assets.register('css_all', css)
 
 
 from app import routes, models, server_api
