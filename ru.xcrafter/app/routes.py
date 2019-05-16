28978d07dd7bdd290b0eeedcb5507920105fddae
espstan <espstan@gmail.com>
2019-05-16 13:19:56 +0300

    ISSUE-206 Добавлен роут load для загрузки в БД

--- ru.xcrafter/app/routes.py
+++ ru.xcrafter/app/routes.py
@@ -1,5 +1,6 @@
 from app import app
 from app import db
+from app import load_test_data
 
 from app.db_utils.products import get_all_products
 from app.db_utils.products import get_product_by_id
@@ -138,6 +139,12 @@ def user():
 @app.route('/jsons/document.json')
 def products():
     return send_from_directory('static', 'jsons/document.json')
+
+
+@app.route('/load')
+def load_data():
+    load_test_data.load_data()
+    return redirect(url_for('index'))
 
 
 @app.route('/api/1/login', methods=['POST'])
