0e58eac1e905cb1c985e1e3841cec693d9b2fdde
espstan <espstan@gmail.com>
2019-05-16 15:49:42 +0300

    ISSUE-206 Переименованы пути, добавлены исключения при работе с БД

--- ru.xcrafter/app/load_test_data.py
+++ ru.xcrafter/app/load_test_data.py
@@ -3,6 +3,8 @@ from app import app
 from app.models import Users
 from app.models import Products
 
+from flask_sqlalchemy import SQLAlchemy
+
 from app import db
 
 from werkzeug.security import generate_password_hash
@@ -11,10 +13,10 @@ import json
 
 
 def load_data():
-    test_user_path = app.root_path + 'app/static/jsons/user.json'
-    test_products_path = app.root_path + 'app/static/jsons/document.json'
+    test_user_data_path = app.root_path + 'app/static/jsons/user.json'
+    test_products_data_path = app.root_path + 'app/static/jsons/document.json'
 
-    with open(test_user_path) as user_json:
+    with open(test_user_data_path) as user_json:
         data = json.load(user_json)[0]
         pass_hash = generate_password_hash(data['password'])
         user = Users(first_name=data['name'],
@@ -24,10 +26,13 @@ def load_data():
                      password_hash=pass_hash,
                      agree_to_processing_personal_data=True,
                      active=data['active'])
-        db.session.add(user)
-        db.session.commit()
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except SQLAlchemy.OperationalError:
+            return
 
-    with open(test_products_path) as prod_json:
+    with open(test_products_data_path) as prod_json:
         data = json.load(prod_json)
         for prod in data:
             product = Products(title=prod['title'],
@@ -35,5 +40,8 @@ def load_data():
                                price=prod['price'],
                                photo=prod['photo'],
                                seller_id=prod['seller'])
-            db.session.add(product)
-            db.session.commit()
+            try:
+                db.session.add(product)
+                db.session.commit()
+            except SQLAlchemy.OperationalError:
+                return
