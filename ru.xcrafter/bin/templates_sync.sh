56f4b9cba591b062f072cd2508c3dc80ee96010f
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-05-16 13:38:07 +0300

    ISSUE-222 Добавление скрипта синхронизации шаблонов templates_sync.sh
    Добавлен скрипт синхронизации шаблонов
    xcrafter/ru.xcrafter/bin/templates_sync.sh -
    запуск из корневой папки xcrafter.

---
+++ ru.xcrafter/bin/templates_sync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rsync -av --chown=xcrafter:xcrafter --delete \
+  ./ru.xcrafter/app/templates/ \
+  xcrafter@xcrafter.ru:/home/xcrafter/production/xcrafter.ru/ru.xcrafter/app/templates/
+
