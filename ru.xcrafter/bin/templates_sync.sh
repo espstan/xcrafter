82cba185ba0c152dc91472de3387f9f3de603328
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-05-16 15:16:50 +0300

    ISSUE-236 Исправление скрипта templates_sync.sh
    Исправлен путь назначения для копирования директории templates.

--- ru.xcrafter/bin/templates_sync.sh
+++ ru.xcrafter/bin/templates_sync.sh
@@ -2,5 +2,5 @@
 
 rsync -av --chown=xcrafter:xcrafter --delete \
   ./ru.xcrafter/app/templates/ \
-  xcrafter@xcrafter.ru:/home/xcrafter/production/xcrafter.ru/ru.xcrafter/app/templates/
+  xcrafter@xcrafter.ru:/home/xcrafter/production/xcrafter.ru/app/templates/
 
