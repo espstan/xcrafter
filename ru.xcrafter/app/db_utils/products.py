d5e8b473dc3e3b5b53cd2d373895b91ff8da89ae
Dmitriy <golub.d.a@yandex.ru>
2019-05-17 11:28:54 +0300

    ISSUE-251 Изменил имя модуля в db.utils/products.py

--- ru.xcrafter/app/db_utils/products.py
+++ ru.xcrafter/app/db_utils/products.py
@@ -1,6 +1,6 @@
 from app import db
 
-from app.models import Products
+from app.models import Product
 
 from flask_login import current_user
 
@@ -12,7 +12,7 @@ from sqlalchemy.exc import OperationalError
 def get_all_products() -> []:
     """Возвращает массив словарей(товаров)"""
 
-    data = Products.query.all()
+    data = Product.query.all()
     products = []
 
     for product in data:
@@ -28,7 +28,7 @@ def get_product_by_id(product_id: int) -> {}:
     При запросе по не существующему id - возвращает пустой словарь."""
 
     try:
-        data = Products.query.filter(Products.id == product_id).one()
+        data = Product.query.filter(Product.id == product_id).one()
         product = {'id': data.id, 'title': data.title, 'description': data.description,
                    'price': data.price, 'photo': [data.photo], 'seller_id': data.seller_id}
     except NoResultFound:
@@ -42,7 +42,7 @@ def add_product(product: {}) -> str:
     Принимает словарь {'title': '', 'description': '', 'price': '', 'photo': '', 'seller_id': ''}.
     Возвращает строку ok/err -  добавилось/база не доступна"""
 
-    product_for_add = Products(product['title'], product['description'], product['price'], product['photo'],
+    product_for_add = Product(product['title'], product['description'], product['price'], product['photo'],
                                product['seller_id'])
 
     db.session.add(product_for_add)
@@ -60,7 +60,7 @@ def delete_product_by_id(product_id: int):
     """Удаляет продукт из базы. Принимает id товара."""
 
     try:
-        product_for_delete = Products.query.filter(Products.id == product_id).one()
+        product_for_delete = Product.query.filter(Product.id == product_id).one()
     except NoResultFound:
         # Пока не знаю как обработать
         return
@@ -78,7 +78,7 @@ def edit_product(item: {}):
      (не обязательно все ключи)"""
 
     try:
-        edited_item = Products.query.filter(Products.id == item['id']).one()
+        edited_item = Product.query.filter(Product.id == item['id']).one()
 
         if 'title' in item.keys():
             edited_item.title = item['title']
