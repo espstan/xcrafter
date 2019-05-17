9167d27bfe9d82c7894bff804a4261cf1a7993a4
espstan <espstan@gmail.com>
2019-05-17 16:51:56 +0300

    ISSUE-206 Изменён способ получения пути к файлам

--- ru.xcrafter/db_fill_from_json.py
+++ ru.xcrafter/db_fill_from_json.py
@@ -1,42 +1,47 @@
+from os.path import join
+
 import click
 
 import json
 
 from app import app
+from app import db
 
 from werkzeug.security import generate_password_hash
 
 from sqlalchemy.exc import SQLAlchemyError
 
-from app import db
-
 from app.models import Users
 from app.models import Products
 
 
-@click.command()
+@click.command(help='Загрузить демо-товары в базу данных')
 def load():
     """
     Загрузка тестовых данных в базу данных
     """
-    test_user_data_path = app.root_path + '/static/jsons/user.json'
-    test_products_data_path = app.root_path + '/static/jsons/document.json'
+    path = app.root_path
+    path1 = '/static/jsons/user.json'
+    path2 = '/static/jsons/document.json'
+
+    test_user_data_path = join(path, path1)
+    test_products_data_path = join(path, path2)
     with open(test_user_data_path) as user_json:
         data = json.load(user_json)[0]
-        pass_hash = generate_password_hash(data['password'])
-        user = Users(first_name=data['name'],
-                     surname=data['surname'],
-                     email=data['email'],
-                     phone_number=data['phone'],
-                     password_hash=pass_hash,
-                     agree_to_processing_personal_data=True,
-                     active=data['active'])
-        try:
-            db.session.add(user)
-            db.session.commit()
-        except SQLAlchemyError as e:
-            error = str(e.__class__.__name__)
-            click.echo("SQLAlchemy error: " + error)
+    pass_hash = generate_password_hash(data['password'])
+    user = Users(first_name=data['name'],
+                 surname=data['surname'],
+                 email=data['email'],
+                 phone_number=data['phone'],
+                 password_hash=pass_hash,
+                 agree_to_processing_personal_data=True,
+                 active=data['active'])
+    try:
+        db.session.add(user)
+        db.session.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__class__.__name__)
+        click.echo("SQLAlchemy error: " + error)
 
     with open(test_products_data_path) as prod_json:
         data = json.load(prod_json)
