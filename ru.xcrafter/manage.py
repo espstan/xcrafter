48672477d437af0e3780dea1024c08c19790234d
Dmitriy <golub.d.a@yandex.ru>
2019-05-17 13:43:37 +0300

    ISSUE-127 Добавил импорт моделей в manage.py

--- ru.xcrafter/manage.py
+++ ru.xcrafter/manage.py
@@ -5,6 +5,12 @@ from flask_migrate import Migrate, MigrateCommand
 from app import app
 from app import db
 
+from app.models import User
+from app.models import Product
+from app.models import Order
+from app.models import Adress
+from app.models import Photo
+
 migrate = Migrate(app, db)
 
 manager = Manager(app)
