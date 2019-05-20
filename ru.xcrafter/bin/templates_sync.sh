ffe284b61ee723a82e1064e3b5d77cbdaf440d47
Sergei Kurmenev <sergeykurmenev@gmail.com>
2019-05-20 10:43:32 +0300

    ISSUE-289 Добавление `set -x` во все скрипты
    Добавлен `set -x` в скрипт bin/templates_sync.sh

--- ru.xcrafter/bin/templates_sync.sh
+++ ru.xcrafter/bin/templates_sync.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 rsync -av --chown=xcrafter:xcrafter --delete \
   ./ru.xcrafter/app/templates/ \
   xcrafter@xcrafter.ru:/home/xcrafter/production/xcrafter.ru/app/templates/
