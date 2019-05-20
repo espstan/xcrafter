d556358bbcafee9603a155e46d6935c501449d8e
espstan <espstan@gmail.com>
2019-05-20 16:48:24 +0300

    ISSUE-216 Внесены изменения в routes.py

--- ru.xcrafter/app/routes.py
+++ ru.xcrafter/app/routes.py
@@ -71,13 +71,13 @@ def profile():
                            current_user=current_user)
 
 
-@app.route('/profile/add-сard-product')
+@app.route('/profile/add-product')
 @login_required
 def add_card_item():
     return render_template('add-card-item.html')
 
 
-@app.route('/profile/edit-card-product/<product_id>')
+@app.route('/profile/<product_id>/edit-product/')
 @login_required
 def profile_edit_card_item(product_id):
     try:
@@ -115,7 +115,7 @@ def card_item(product_id):
         logger.warning('Ошибка при обращении к БД товаров: {}'.format(e))
 
 
-@app.route('/product/photo/<product_id>')
+@app.route('/product/<product_id>/photo')
 def card_item_photo(product_id):
     try:
         product = get_product_by_id(product_id)
@@ -135,7 +135,7 @@ def card_item_description(product_id):
         logger.warning('Ошибка при обращении к БД товаров: {}'.format(e))
 
 
-@app.route('/product/about-seller/<product_id>')
+@app.route('/product/<product_id>/about-seller')
 def card_item_about_seller(product_id):
     try:
         product = get_product_by_id(product_id)
