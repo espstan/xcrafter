c84f37ad498da8ff7f3d9cdf7b3e5e07ebe270eb
espstan <espstan@gmail.com>
2019-05-20 12:57:45 +0300

    ISSUE-206 Исправления автоформата

--- ru.xcrafter/commands.py
+++ ru.xcrafter/commands.py
@@ -38,20 +38,20 @@ def load(demo):
     test_user_data_path = join(app.root_path, 'static', 'jsons', 'user.json')
     with open(test_user_data_path) as user_json:
         data = json.load(user_json)[0]
-    pass_hash = generate_password_hash(data['password'])
-    user = Users(first_name=data['name'],
-                 surname=data['surname'],
-                 email=data['email'],
-                 phone_number=data['phone'],
-                 password_hash=pass_hash,
-                 agree_to_processing_personal_data=True,
-                 active=data['active'])
-    try:
-        db.session.add(user)
-        db.session.commit()
-    except SQLAlchemyError as e:
-        error = str(e.__class__.__name__)
-        click.echo("SQLAlchemy error: " + error)
+        pass_hash = generate_password_hash(data['password'])
+        user = Users(first_name=data['name'],
+                     surname=data['surname'],
+                     email=data['email'],
+                     phone_number=data['phone'],
+                     password_hash=pass_hash,
+                     agree_to_processing_personal_data=True,
+                     active=data['active'])
+        try:
+            db.session.add(user)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            error = str(e.__class__.__name__)
+            click.echo("SQLAlchemy error: " + error)
 
 
 if __name__ == '__main__':
