4b567fa8976bca2b78a2380f63613d92da2ca84a
espstan <espstan@gmail.com>
2019-05-22 17:00:41 +0300

    ISSUE-308 Удалён неиспользуемый класс

--- ru.xcrafter/app/forms.py
+++ ru.xcrafter/app/forms.py
@@ -2,8 +2,7 @@ from flask_wtf import FlaskForm
 
 from wtforms import StringField 
 from wtforms import PasswordField 
-from wtforms import BooleanField
-from wtforms import SubmitField 
+from wtforms import SubmitField
 
 from wtforms.validators import DataRequired
 from wtforms.validators import Email
@@ -12,13 +11,8 @@ from wtforms.validators import Email
 class LoginForm(FlaskForm):
     user_first_name = StringField('Имя', validators=[DataRequired()])
     user_surname = StringField('Фамилия', validators=[DataRequired()])
-    user_email = StringField('E-mail', validators=[DataRequired()])
+    user_email = StringField('E-mail', validators=[DataRequired(), Email()])
     user_phone_number = StringField('Телефон', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     submit = SubmitField('Отправить')
-
-
-class AddSubscriptionRequestForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Apply')
 
