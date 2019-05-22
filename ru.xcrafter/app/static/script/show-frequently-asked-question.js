6744512d111a51c8c6be7a4f45e7836781c40299
Darina <darinakuleva@gmail.com>
2019-05-22 18:36:48 +0300

    ISSUE-312 Ренейминг картинок

--- ru.xcrafter/app/static/script/show-frequently-asked-question.js
+++ ru.xcrafter/app/static/script/show-frequently-asked-question.js
@@ -1,9 +1,9 @@
 showAnswer = ( idBtnShow, idQuestion ) => {
   if ( document.getElementById( idQuestion ).style.color === 'black' ) {
     document.getElementById( idQuestion ).style.color = '#3254c9';
-    document.getElementById( idBtnShow ).src = '/static/img/Question-close.svg'
+    document.getElementById( idBtnShow ).src = '/static/img/question_close.svg'
   } else {
     document.getElementById( idQuestion ).style.color = 'black';
-    document.getElementById( idBtnShow ).src = '/static/img/Question-show.svg'
+    document.getElementById( idBtnShow ).src = '/static/img/question_show.svg'
   }
 };
