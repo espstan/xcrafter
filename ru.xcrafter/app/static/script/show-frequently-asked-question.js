b071dd797f573c3291f37fe1a9e1381ce820d0f1
Darina <darinakuleva@gmail.com>
2019-05-22 18:20:05 +0300

    ISSUE-312 Перенос верстки на Bootstrap

--- ru.xcrafter/app/static/script/show-frequently-asked-question.js
+++ ru.xcrafter/app/static/script/show-frequently-asked-question.js
@@ -1,13 +1,9 @@
-showAnswer = (idAnswer, idQuestion, idBtnShow) => {
-  if (document.getElementById(idAnswer).style.display === 'none') {
-    document.getElementById(idAnswer).style.display = 'block';
-    document.getElementById(idQuestion).style.color = '#3254c9';
-    document.getElementById(idBtnShow).src = '../static/img/Question-close.svg'
-
-  }
-  else {
-    document.getElementById(idAnswer).style.display = 'none';
-    document.getElementById(idQuestion).style.color = 'black';
-    document.getElementById(idBtnShow).src = '../static/img/Question-show.svg'
+showAnswer = ( idBtnShow, idQuestion ) => {
+  if ( document.getElementById( idQuestion ).style.color === 'black' ) {
+    document.getElementById( idQuestion ).style.color = '#3254c9';
+    document.getElementById( idBtnShow ).src = '/static/img/Question-close.svg'
+  } else {
+    document.getElementById( idQuestion ).style.color = 'black';
+    document.getElementById( idBtnShow ).src = '/static/img/Question-show.svg'
   }
 };
