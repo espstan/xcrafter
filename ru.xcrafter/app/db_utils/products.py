0e16d1d4c19265a5e139554554a3a4d4844fff33
Dmitriy <golub.d.a@yandex.ru>
2019-05-22 14:20:26 +0300

    ISSUE-221 Изменил название метода добавления фото в ДБ

--- ru.xcrafter/app/db_utils/products.py
+++ ru.xcrafter/app/db_utils/products.py
@@ -118,7 +118,7 @@ def edit_product(item: {}):
         pass
 
 
-def upload_photo_in_db(path, user_id, product_id):
+def upload_photo(path, user_id, product_id):
     photo = Photo(product_id, user_id, path)
     db.session.add(photo)
 
