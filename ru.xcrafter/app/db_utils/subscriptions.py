59a35c48ffc8e2fe2001614cdaa51f58251230d0
espstan <espstan@gmail.com>
2019-05-22 14:07:52 +0300

    ISSUE-308 В создан модуль для класса Subscription

---
+++ ru.xcrafter/app/db_utils/subscriptions.py
@@ -0,0 +1,27 @@
+import sqlalchemy
+
+from app import db
+
+from sqlalchemy.exc import InvalidRequestError
+
+from sqlalchemy.orm.exc import NoResultFound
+
+from app.models import Subscription
+
+
+def get_subscription(email):
+    try:
+        subscription = Subscription.query.filter(Subscription.email == email).one()
+    except NoResultFound:
+        return None
+    return subscription
+
+
+def add_subscription(email):
+    try:
+        db.session.add(Subscription(email))
+        db.session.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__class__.__name__)
+        print("SQLAlchemy error: " + error)
+    print("Success")
