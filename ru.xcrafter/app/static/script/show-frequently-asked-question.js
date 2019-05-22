d37af9cde970b70ca5dbd6f2157b77e3511fb72e
Darina <darinakuleva@gmail.com>
2019-05-22 15:33:01 +0300

    ISSUE-312 Добавление появления ответа при нажатии на вопрос

--- ru.xcrafter/app/static/script/show-frequently-asked-question.js
+++ ru.xcrafter/app/static/script/show-frequently-asked-question.js
@@ -1,2 +1,13 @@
-showQuestion = () => {
-}
+showAnswer = (idAnswer, idQuestion, idBtnShow) => {
+  if (document.getElementById(idAnswer).style.display === 'none') {
+    document.getElementById(idAnswer).style.display = 'block';
+    document.getElementById(idQuestion).style.color = '#3254c9';
+    document.getElementById(idBtnShow).src = '../static/img/Question-close.svg'
+
+  }
+  else {
+    document.getElementById(idAnswer).style.display = 'none';
+    document.getElementById(idQuestion).style.color = 'black';
+    document.getElementById(idBtnShow).src = '../static/img/Question-show.svg'
+  }
+};
