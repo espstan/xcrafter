5582fab048bb88c189359ef7c675cf06784b63bf
Darina <darinakuleva@gmail.com>
2019-05-23 14:22:06 +0300

    ISSUE-369 Рефакторинг base.html

--- ru.xcrafter/xcrafter/static/script/show-frequently-asked-question.js
+++ ru.xcrafter/xcrafter/static/script/show-frequently-asked-question.js
@@ -1,9 +1,14 @@
-showAnswer = ( idBtnShow, idQuestion ) => {
-  if ( document.getElementById( idQuestion ).style.color === 'black' ) {
-    document.getElementById( idQuestion ).style.color = '#3254c9';
-    document.getElementById( idBtnShow ).src = '/static/img/question_close.svg'
+showAnswer = ( questionNumber ) => {
+  const questionIndex = questionNumber - 1;
+  const elem = $( "button.question-title:eq(" + questionIndex + ")" );
+  const questionsBlock = document.getElementById( 'frequently-asked-question' );
+  const buttonOpenAnswer = questionsBlock.getElementsByTagName( 'img' )[questionIndex];
+
+  if ( elem.css( 'color' ) === 'rgb(0, 0, 0)' ) {
+    elem.css( "color", "#3254c9" );
+    buttonOpenAnswer.src = '/static/img/question_close.svg';
   } else {
-    document.getElementById( idQuestion ).style.color = 'black';
-    document.getElementById( idBtnShow ).src = '/static/img/question_show.svg'
+    elem.css( "color", "black" );
+    buttonOpenAnswer.src = '/static/img/question_show.svg';
   }
 };
