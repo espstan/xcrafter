5c8668634616970ef834781d176fcc770b8dbf8d
espstan <espstan@gmail.com>
2019-05-31 12:50:45 +0300

    ISSUE0469  Внесены изменения в функцию send_mail

--- ru.xcrafter/xcrafter/db_utils/email_utils.py
+++ ru.xcrafter/xcrafter/db_utils/email_utils.py
@@ -1,13 +1,21 @@
-from flask_mail import Message
-
+from xcrafter import app
 from xcrafter import mail
 
+from flask_mail import Message
 
-def send_mail(message_theme, message_body, sender,  *recipients):
+
+def send_mail(message_theme, message_html, email):
     try:
-        msg = Message(message_theme, sender, recipients)
-        msg.body = message_body
-        mail.send(msg)
-        return "Mail sent, success!"
+        msg = Message(message_theme,
+                      sender="juniorlabtest@gmail.com",
+                      recipients=[email])
+        msg.title = message_theme
+        msg.html = message_html
+        print(msg.title)
+        print(email)
+
+        with app.app_context():
+            mail.send(msg)
+        print("Mail sent, success!")
     except Exception as e:
         return str(e)
